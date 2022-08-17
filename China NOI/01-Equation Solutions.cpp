6f746a03f783d39a3c490c2c970752f0b6f93512
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-08-16 22:31:21 -0700

    Update 01-Equation Solutions.cpp

--- China NOI/01-Equation Solutions.cpp
+++ China NOI/01-Equation Solutions.cpp
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
-#pragma GCC target ("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
 using namespace std;
 vector<int> ma;
 vector<int> p, k;
 int M;
 int res = 0;
-bool b;
-int powr[7][151];
+bool b = true;
 vector<pair<int,int> > v;
 void generate() {
     while (!ma.empty()) {
@@ -26,14 +22,13 @@ int bs (int x) {
     int r = v.size() - 1;
     while (l != r) {
         int m = (l + r)/2;
-        if (v[m].first >= x) {
+        if (v[m].first <= x) {
             r = m;
         } else {
             l = m + 1;
         }
     }
-    if (v[l].first == x) return v[l].second;
-    return 0;
+    return ((v[l].first != x) ? 0 : v[l].second);
 }
 void rec (int sz, int ans) {
     if (sz == p.size()) {
@@ -42,35 +37,27 @@ void rec (int sz, int ans) {
         } else {
             res += bs(-ans);
         }
-        return;
+    } else {
+        for (int i = 1; i <= M; i++) {
+            rec(sz + 1, ans + k[sz] * pow(i, p[sz]));
+        }
     }
-    for (int i = 1; i <= M; i++) {
-        rec(sz + 1, ans + k[sz] * powr[p[sz]][i]);
-    }
-}
-bool comp (pair<int,int> p, pair<int,int> q) {
-    if (q.second == p.second) {
-        return (q.first < p.first);
-    }
-    return (q.second < p.second);
 }
 int main() {
-    for (int i = 1; i < 7; i++) for (int j = 1; j <= 150; j++) powr[i][j] = pow(j, i);
 	int n;
     cin >> n >> M;
     vector<pair<int,int> > vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i].first >> vec[i].second;
     for (int i = 0; i < n/2; i++) {
-    	p.push_back(vec[i].second), k.push_back(vec[i].first);
+        int K, P; cin >> K >> P;
+    	p.push_back(P), k.push_back(K);
     }
-    b = true;
     rec(0, 0);
     sort(ma.begin(), ma.end());
     generate();
-    reverse(v.begin(), v.end());
     p.clear(), k.clear();
     for (int i = n/2; i < n; i++) {
-    	p.push_back(vec[i].second), k.push_back(vec[i].first);
+    	int K, P; cin >> K >> P;
+    	p.push_back(P), k.push_back(K);
     }
     b = false;
     rec(0, 0);
