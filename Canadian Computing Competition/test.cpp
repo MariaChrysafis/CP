8bf75f7650118c656158e3461db2a8164f3e36cd
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-10-08 09:14:52 -0700

    Update test.cpp

--- Canadian Computing Competition/test.cpp
+++ Canadian Computing Competition/test.cpp
@@ -20,14 +20,23 @@ long long sum_subtriangles (int N, int K, vector<vector<int> > arr) {
             arr[i].push_back(0);
         }
     }
+    int ind[k + 1];
+    for (int i = 0; i <= k; i++) {
+        ind[i] = -1;
+    }
+    int c = 0;
+    for (int x: s) {
+        ind[x] = c++;
+        ind[x] %= 2;
+    }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j <= i; j++) {
             up[i][j][0] = down[i][j][0] = arr[i][j];
         }        
     }
-    int a = 0;
+    s.erase(1);
     for (int x: s) {
-        a++, a %= 2;
+        int a = ind[x];
         for (int i = x - 1; i < n; i++) {
             for (int j = 0; j + x - 1 <= i; j++) {
                 if (x % 2 == 0) {
@@ -41,10 +50,10 @@ long long sum_subtriangles (int N, int K, vector<vector<int> > arr) {
         for (int i = 0; i + x - 1 < n; i++) {
             for (int j = x - 1; j <= i; j++) {
                 if (x % 2 == 0) {
-                    down[i][j][a] = max(max(down[i + x/2][j][!a], down[i][j][!a]), max(down[i][j - x/2][!a], up[i + (x/2 - 1)][j - (x/2 - 1)][!a]));
+                    down[i][j][a] = max(max(down[i + x/2][j][!a], down[i][j][!a]), max(down[i][j - x/2][!a], up[i + (x/2 - 1)][j - (x/2 - 1)][ind[x/2]]));
                 } else {
-                    down[i][j][a] = max(max(down[i][j - (x/2 + 1)][!a], down[i + (x/2 + 1)][j][!a]), max(down[i][j][!a], up[i + x/2][j - x/2][!a]));
-                    down[i][j][a] = max(down[i][j][a], max(down[i + 1][j][!a], down[i][j - 1][!a]));
+                    down[i][j][a] = max(max(down[i][j - (x/2 + 1)][!a], down[i + (x/2 + 1)][j][!a]), max(down[i][j][ind[x/2]], up[i + x/2][j - x/2][ind[x/2]]));
+                    down[i][j][a] = max(down[i][j][ind[x]], max(down[i + 1][j][ind[x/2]], down[i][j - 1][!a]));
                 }
             }
         }
@@ -52,7 +61,7 @@ long long sum_subtriangles (int N, int K, vector<vector<int> > arr) {
     long long ans = 0;
     for (int i = k - 1; i < n; i++) {
         for (int j = 0; j + k - 1 <= i; j++) {
-            ans += up[i][j][a];
+            ans += up[i][j][ind[k]];
         }
     }
     return ans;
