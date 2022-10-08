5d1d6812e483fd06e989aa6d4c4adb8b432e711c
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-10-08 08:33:13 -0700

    Update test.cpp

--- Canadian Computing Competition/test.cpp
+++ Canadian Computing Competition/test.cpp
@@ -36,23 +36,24 @@ long long sum_subtriangles (int N, int K, vector<vector<int> > arr) {
     }
     s.erase(1);
     for (int x: s) {
+        int a = ind[x];
         for (int i = x - 1; i < n; i++) {
             for (int j = 0; j + x - 1 <= i; j++) {
                 if (x % 2 == 0) {
-                    up[i][j][ind[x]] = max(max(up[i - x/2][j][ind[x/2]], up[i][j][ind[x/2]]), max(down[i - x/2 + 1][j + x/2 - 1][ind[x/2]], up[i][j + x/2][ind[x/2]]));
+                    up[i][j][a] = max(max(up[i - x/2][j][!a], up[i][j][!a]), max(down[i - x/2 + 1][j + x/2 - 1][!a], up[i][j + x/2][!a]));
                 } else {
-                    up[i][j][ind[x]] = max(max(up[i][j][ind[x/2]], down[i - x/2][j + x/2][ind[x/2]]), max(up[i][j + x/2 + 1][ind[x/2]], up[i - (x/2 + 1)][j][ind[x/2]]));
-                    up[i][j][ind[x]] = max(up[i][j][ind[x]], max(up[i - 1][j][ind[x/2]], up[i][j + 1][ind[x/2]]));
+                    up[i][j][a] = max(max(up[i][j][!a], down[i - x/2][j + x/2][!a]), max(up[i][j + x/2 + 1][!a], up[i - (x/2 + 1)][j][!a]));
+                    up[i][j][a] = max(up[i][j][a], max(up[i - 1][j][!a], up[i][j + 1][!a]));
                 }
             }
         }
         for (int i = 0; i + x - 1 < n; i++) {
             for (int j = x - 1; j <= i; j++) {
                 if (x % 2 == 0) {
-                    down[i][j][ind[x]] = max(max(down[i + x/2][j][ind[x/2]], down[i][j][ind[x/2]]), max(down[i][j - x/2][ind[x/2]], up[i + (x/2 - 1)][j - (x/2 - 1)][ind[x/2]]));
+                    down[i][j][a] = max(max(down[i + x/2][j][!a], down[i][j][!a]), max(down[i][j - x/2][!a], up[i + (x/2 - 1)][j - (x/2 - 1)][ind[x/2]]));
                 } else {
-                    down[i][j][ind[x]] = max(max(down[i][j - (x/2 + 1)][ind[x/2]], down[i + (x/2 + 1)][j][ind[x/2]]), max(down[i][j][ind[x/2]], up[i + x/2][j - x/2][ind[x/2]]));
-                    down[i][j][ind[x]] = max(down[i][j][ind[x]], max(down[i + 1][j][ind[x/2]], down[i][j - 1][ind[x/2]]));
+                    down[i][j][a] = max(max(down[i][j - (x/2 + 1)][!a], down[i + (x/2 + 1)][j][!a]), max(down[i][j][ind[x/2]], up[i + x/2][j - x/2][ind[x/2]]));
+                    down[i][j][a] = max(down[i][j][ind[x]], max(down[i + 1][j][ind[x/2]], down[i][j - 1][!a]));
                 }
             }
         }
