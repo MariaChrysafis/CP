c2fdffb5d0c891fc6bcd2f86aead219dc6bc199b
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2022-10-08 08:21:32 -0700

    Update test.cpp

--- Canadian Computing Competition/test.cpp
+++ Canadian Computing Competition/test.cpp
@@ -29,13 +29,17 @@ long long sum_subtriangles (int N, int K, vector<vector<int> > arr) {
         ind[x] = c++;
         ind[x] %= 2;
     }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            up[i][j][0] = down[i][j][0] = arr[i][j];
+        }        
+    }
+    s.erase(1);
     for (int x: s) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                     if (i - (x - 1) >= 0 and j + (x - 1) < n) {
-                        if (x == 1) {
-                            up[i][j][ind[x]] = arr[i][j];
-                        } else if (x % 2 == 0) {
+                        if (x % 2 == 0) {
                             up[i][j][ind[x]] = max(max(up[i - x/2][j][ind[x/2]], up[i][j][ind[x/2]]), max(down[i - x/2 + 1][j + x/2 - 1][ind[x/2]], up[i][j + x/2][ind[x/2]]));
                         } else {
                             up[i][j][ind[x]] = max(max(up[i][j][ind[x/2]], down[i - x/2][j + x/2][ind[x/2]]), max(up[i][j + x/2 + 1][ind[x/2]], up[i - (x/2 + 1)][j][ind[x/2]]));
@@ -43,9 +47,7 @@ long long sum_subtriangles (int N, int K, vector<vector<int> > arr) {
                         }
                     }
                     if (i + (x - 1) < n and j - (x - 1) >= 0) {
-                        if (x == 1) {
-                            down[i][j][ind[x]] = arr[i][j];
-                        } else if (x % 2 == 0) {
+                        if (x % 2 == 0) {
                             down[i][j][ind[x]] = max(max(down[i + x/2][j][ind[x/2]], down[i][j][ind[x/2]]), max(down[i][j - x/2][ind[x/2]], up[i + (x/2 - 1)][j - (x/2 - 1)][ind[x/2]]));
                         } else {
                             down[i][j][ind[x]] = max(max(down[i][j - (x/2 + 1)][ind[x/2]], down[i + (x/2 + 1)][j][ind[x/2]]), max(down[i][j][ind[x/2]], up[i + x/2][j - x/2][ind[x/2]]));
